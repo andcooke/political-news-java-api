c05e19dda61fe3277d11a2fbedc4ff9abec4b463
A <andcooke@gmail.com>
2023-04-26 15:20:33 -0500

    initial commit

---
+++ political-news-java-api/src/main/java/com/politicalnews/PoliticalNewsJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.politicalnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliticalNewsJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliticalNewsJavaApiApplication.class, args);
+	}
+
+}
---
+++ political-news-java-api/src/test/java/com/politicalnews/PoliticalNewsJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.politicalnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliticalNewsJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
