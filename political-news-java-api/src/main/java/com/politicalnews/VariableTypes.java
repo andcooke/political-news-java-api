4a3dd106eddf2eb856d59ec37febe0066d2ba3e8
A <andcooke@gmail.com>
2023-04-27 14:20:46 -0500

    tests and demos made

---
+++ political-news-java-api/src/main/java/com/politicalnews/VariableTypes.java
@@ -0,0 +1,11 @@
+package com.politicalnews;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+        String name ="Cleopatra";
+        int age = 2016 + 70;
+        double price = 1999.99;
+        boolean truth = true;
+        boolean falseness = !true;
+    }
+}
---
+++ political-news-java-api/src/main/java/com/politicalnews/VariablesPractice.java
@@ -0,0 +1,18 @@
+package com.politicalnews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int integer = 10;
+        double decimal = 0.1;
+        double sum = integer + decimal;
+        // got'em
+        System.out.println(sum);
+
+        String str = "num";
+
+        String strinteger = str + integer;
+
+        // hehehe got'em again
+        System.out.println(strinteger);
+    }
+}
